f7773f15139602bd121f50fc195aa82238e7c1fe
Rajnishkumar16 <119354813+Rajnishkumar16@users.noreply.github.com>
2024-11-03 06:20:31 +0530

    completed password generator  mini project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import PasswordGenerator from "./component/PasswordGenerator";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline bg-yellow-300">Password generator</h1>
+      <PasswordGenerator />
     </>
   );
 }
